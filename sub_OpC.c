72e88311c5d8325be1f6c65e2953410cc1b8ad6d
HackiodTech <136133966+HackiodTech@users.noreply.github.com>
2023-12-14 06:25:13 +0000

    first upload

---
+++ sub_OpC.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * f_sub -> Subtracts the first from the second element on the stack and stores
+ * their value in the second number of the stack which now becomes the first
+ * @stack: The stack
+ * @line_number: The line number
+ */
+
+void f_sub(stack_t **stack, unsigned int line_number)
+{
+    stack_t *head_copy = *stack;
+    int length = check_length(head_copy), result;
+
+    if (length < 2)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack);
+        exit(EXIT_FAILURE);
+    }
+
+    head_copy = *stack;
+
+    result = head_copy->next->n - head_copy->n;
+    head_copy->next->n = result;
+    *stack = head_copy->next;
+    free(head_copy);
+}
---
+++ swap_OpC.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * f_swap -> Swaps the first two elements on the list,
+ * if the elements are less than 2 , it prints out an error message
+ * @stack: The stack to be operated on
+ * @line_number: The line number
+ */
+void f_swap(stack_t **stack, unsigned int line_number)
+{
+
+    stack_t *head_copy = *stack;
+    int length = check_length(head_copy), auxilary;
+
+    if (length < 2)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack);
+        exit(EXIT_FAILURE);
+    }
+    head_copy = *stack;
+    auxilary = head_copy->n;
+    head_copy->n = head_copy->next->n;
+    head_copy->next->n = auxilary;
+}
